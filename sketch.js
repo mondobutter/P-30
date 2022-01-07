f6f7099bed91f826d0a2d4a23d95dfb4766d2e31
mondobutter <72702945+mondobutter@users.noreply.github.com>
2022-01-07 16:31:38 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,122 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground(450,400,500,10);
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,250,10); 
+
+  
+ 
+  //level one
+  block1 = new Box(300,275,30,40);
+  console.log(block1);
+  block2 = new Box(330,275,30,40);
+  block3 = new Box(360,275,30,40);
+  block4 = new Box(390,275,30,40);
+  block5 = new Box(420,275,30,40);
+  block6 = new Box(450,275,30,40);
+  block7 = new Box(480,275,30,40);
+  block17 = new Box(610,190,30,40);
+  block18 = new Box(640,190,30,40);
+  block19 = new Box(670,190,30,40);
+  block20 = new Box(700,190,30,40);
+  block21 = new Box(730,190,30,40);
+  block22 = new Box(760,190,30,40);
+  block23 = new Box(790,190,30,40);
+  //level two
+  block8 = new Box(330,235,30,40);
+  block9 = new Box(360,235,30,40);
+  block10 = new Box(390,235,30,40);
+  block11 = new Box(420,235,30,40);
+  block12 = new Box(450,235,30,40);
+  block24 = new Box(640,100,30,40);
+  block25 = new Box(670,100,30,40);
+  block26 = new Box(700,100,30,40);
+  block27 = new Box(730,100,30,40);
+  block28 = new Box(760,100,30,40); 
+  //level three
+  block13 = new Box(360,195,30,40);
+  block14 = new Box(390,195,30,40);
+  block15 = new Box(420,195,30,40);
+  block29 = new Box(670,90,30,40);
+  block30 = new Box(700,90,30,40);
+  block31 = new Box(730,90,30,40);
+  //top
+  block16 = new Box(390,155,30,40);
+  block32 = new Box(700,70,30,40);
+
+
+
+
+  
+
+}
+function draw() {
+  background(56,44,44); 
+ 
+  textSize(20);
+  fill("lightyellow");
+  
+
+  ground.display();
+  stand1.display();
+  stand2.display();
+  
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block17.display();
+  block18.display();
+  block19.display();
+  block20.display();
+  block21.display();
+  block22.display();
+  block23.display();
+  fill("pink");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block24.display();
+  block25.display();
+  block26.display();
+  block27.display();
+  block28.display();
+  fill("turquoise");
+  block13.display();
+  block14.display();
+  block15.display();
+  block29.display();
+  block30.display();
+  block31.display();
+  fill("grey");
+  block16.display();
+  block32.display();
+  
+ 
+
+}
